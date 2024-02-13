c1d584489056589219a19f18368f8e0850263361
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-02-13 18:24:03 -0300

    :construction: exercise_0401

--- classes/module_04/0401_Constructions_Functions_2/scripts/script.js
+++ classes/module_04/0401_Constructions_Functions_2/scripts/script.js
@@ -1,8 +1,25 @@
-function Carro() {
-  this.marca = "Marca";
-  this.preco = 90000;
+// const Dom = {
+//   // Abaixo estmos criando uma propriedade chamada de seletor e seu valor é r `li`
+//   seletor: "li",
+//   // Abaixo vamos ter um funçào (método) chamado de element para retornar o nosso seletor 'li'
+//   element() {
+//     return document.querySelector(this.seletor);
+//   },
+//   // Adicionado a classe ativar dentro do nosso `li`
+//   ativar() {
+//     const elementoSelecionado = this.element();
+//     this.element().classList.add("ativar");
+//   },
+// };
+
+function Dom(seletor) {
+  this.element = function () {
+    return document.querySelector(seletor);
+  };
+  this.ativar = function (classe) {
+    this.element().classList.add(classe);
+  };
 }
 
-const honda = new Carro();
-const fiat = new Carro();
-fiat.marca = "Fiat";
+const ativandoclasse = new Dom("li:last-child");
+ativandoclasse.ativar("acticve");
---
+++ exercises/module_04/exercise_0402/scripts/script.js
@@ -0,0 +1,32 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+
+function Pessoa(nome, sobrenome, idade) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+  const nomeCompleto = this.nome + this.sobrenome;
+  this.nomeCompleto = nomeCompleto;
+}
+const Novapessoa = new Pessoa(nomeCompleto);
+
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector("li");
+
+li;
+li.click;
+li.innerText;
+li.value;
+li.hidden;
+li.offsetLeft;
+li.click();
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;
