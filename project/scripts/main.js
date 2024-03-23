989d23647bbd3506ab08fbcc8478c00bc86741ef
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-03-23 14:42:44 -0300

    refactor: add function accordion-list
    with this commit the accordion function was added to the questions
    
    reviewed: Emmanuel Oliveira

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -20,3 +20,21 @@ function activeTabs() {
   }
 }
 activeTabs();
+//---------------------------------------------------------------------
+// function accordions-list
+//---------------------------------------------------------------------
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  accordionList[0].classList.add("active");
+  accordionList[0].nextElementSibling.classList.add("active");
+  function activeAccordion() {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("active");
+  }
+
+  accordionList.forEach((item) => {
+    item.addEventListener("click", activeAccordion);
+  });
+}
+initAccordion();
+//---------------------------------------------------------------------
