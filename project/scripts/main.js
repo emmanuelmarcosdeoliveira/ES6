d2c04a2b00c3c0ac1e94f1e0ae20f54e434f9365
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-03-23 14:57:59 -0300

    refactor: function accordions-list
    inclusion of the accrrdiosn function
    
    revision: Emmanuel Oliveira

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -25,16 +25,20 @@ activeTabs();
 //---------------------------------------------------------------------
 function initAccordion() {
   const accordionList = document.querySelectorAll(".js-accordion dt");
-  accordionList[0].classList.add("active");
-  accordionList[0].nextElementSibling.classList.add("active");
-  function activeAccordion() {
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("active");
-  }
+  const activeClass = "active";
 
-  accordionList.forEach((item) => {
-    item.addEventListener("click", activeAccordion);
-  });
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
 }
 initAccordion();
 //---------------------------------------------------------------------
